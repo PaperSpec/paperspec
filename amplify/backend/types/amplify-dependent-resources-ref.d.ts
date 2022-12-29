814b6200c061d59a15d31ad816f568e80fd971a3
hodgest <toby.hodges@autodesk.com>
2022-12-29 00:51:11 -0800

    initial skeleton

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "paperspec": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
