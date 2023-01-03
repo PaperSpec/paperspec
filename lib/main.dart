dea91ad009696a23a437bbcb6a32d1c7f464e6a3
hodgest <toby.hodges@autodesk.com>
2023-01-03 09:13:52 -0800

    beginning tutorial following

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_api/amplify_api.dart';
+// import 'package:amplify_storage_s3/amplify_storage_s3.dart';
+// import 'package:amplify_authenticator/amplify_authenticator.dart';
+// import 'package:amplify_datastore/amplify_datastore.dart';
 import 'package:paperspec/models/ModelProvider.dart';
 
 import 'amplifyconfiguration.dart';
